07d40d9d5d4bac63b61b67ff37c7e06dee9173f6
Ivan Zhukov <electrocartridge@gmail.com>
2017-03-18 23:21:43 +0300

    Add version 0.1

---
+++ js/index.js
@@ -0,0 +1,129 @@
+var sockURL = 'http://' + document.location.hostname + '/echo';
+var player;
+
+function socketOpened(reconnectInterval, updateTimeInterval, sock) {
+    $('#connectionState').text('connected');
+    $('#connectionState').css('color', 'green');
+
+    clearInterval(reconnectInterval);
+
+    sock.send({
+        type: 'init'
+    });
+
+    updateTimeInterval = setInterval(function() {
+        sock.send({
+            type  : 'updateTimeInfo',
+            value : player.currentTime
+        });
+    }, 5000);
+}
+
+function socketMessage(event) {
+    if (event.type === 'message') {
+        var action;
+        try {
+            action = JSON.parse(event.data);
+        } catch(error) {
+            console.error('Failed to parse action: ' + event.data);
+            return;
+        }
+
+        if (action.type === 'pause') {
+            player.pause();
+        }
+
+        if (action.type === 'play') {
+            player.play();
+        }
+
+        if (action.type === 'setTime') {
+            console.log('setTime: ' + action.value);
+
+            player.currentTime = action.value;
+        }
+    }
+}
+
+function socketClosed(reconnectInterval, updateTimeInterval, sock) {
+    $('#connectionState').text('not connected');
+    $('#connectionState').css('color', 'red');
+
+    clearInterval(updateTimeInterval);
+    setTimeout(socketLogic, 1000);
+}
+
+var playListener;
+var pauseListener;
+
+function socketLogic() {
+    var sock = new SockJS(sockURL);
+
+    sock.oldSend = sock.send;
+    sock.send = function(objToSend) { // stringify all sent objects
+        sock.oldSend(JSON.stringify(objToSend));
+    };
+    var updateTimeInterval;
+    var reconnectInterval;
+
+    sock.onopen = function() {
+        socketOpened(reconnectInterval, updateTimeInterval, sock);
+    };
+    sock.onmessage = function(event) {
+        socketMessage(event);
+    };
+    sock.onclose = function() {
+        socketClosed(reconnectInterval, updateTimeInterval, sock);
+    };
+
+    $('#goToTimeBtn').off('click');
+    $('#goToTimeBtn').click(function() {
+        var hms = $('#goToTimeInput').val().split(':');
+
+        if (! (hms.length && hms.length === 3)) {
+            alert('wrong time format!');
+            return;
+        }
+
+        var nextTime = parseFloat(hms[0]) * 3600 + parseFloat(hms[1]) * 60 +
+                       parseFloat(hms[2]);
+        sock.send({
+            type  : 'setTime',
+            value : nextTime
+        });
+    });
+
+    if (playListener) {
+        player.removeEventListener('play', playListener);
+    }
+    if (pauseListener) {
+        player.removeEventListener('pause', pauseListener);
+    }
+
+    playListener = function(event) {
+        sock.send({
+            type: 'play'
+        });
+    };
+    pauseListener = function(event) {
+        sock.send({
+            type: 'pause'
+        });
+    };
+    timeUpdateListener = function(event) {
+        sock.send({
+            type  : 'setTime',
+            value : event.target.currentTime
+        });
+    };
+
+    player.addEventListener('play',  playListener);
+    player.addEventListener('pause', pauseListener);
+
+    return sock;
+}
+
+$(document).ready(function() {
+    player = document.getElementById('player');
+    var sock = socketLogic();
+});
---
+++ server.js
@@ -0,0 +1,114 @@
+const http   = require('http');
+const sockjs = require('sockjs');
+const uuid   = require('uuid');
+const bunyan = require('bunyan');
+
+const logger = bunyan.createLogger({
+    name: 'DistributedPlayer',
+    level: 10
+});
+
+let currentTime = 0;
+let times = [];
+let connections = {};
+
+function sendToAll(data) {
+    for (let id in connections) {
+        connections[id].write(JSON.stringify(data));
+    }
+}
+
+function sendToOthers(sourceId, data) {
+    for (let id in connections) {
+        if (id != sourceId) {
+            connections[id].write(JSON.stringify(data));
+        }
+    }
+}
+
+function send(connection, data) {
+    connection.write(JSON.stringify(data));
+}
+
+function updateTimes(newTime) {
+    times.push(newTime);
+    times = times.slice(-3); // last 3 elements
+
+    if (times.length === 3) {
+        const variance12 = Math.abs(times[0] - times[1]);
+        const variance23 = Math.abs(times[1] - times[2]);
+        const variance13 = Math.abs(times[0] - times[2]);
+
+        const maxVariance = Math.max(variance12, variance23, variance13);
+        const minVariance = Math.min(variance12, variance23, variance13);
+
+        if (maxVariance > 2) {
+            // leave only times with minimum variance
+            if (minVariance === variance12) {
+                times = [times[0], times[1]];
+            };
+            if (minVariance === variance23) {
+                times = [times[1], times[2]];
+            };
+            if (minVariance === variance13) {
+                times = [times[0], times[2]];
+            };
+        }
+    }
+
+    currentTime = times.reduce((acc, val) => acc + val, 0) / times.length;
+
+    logger.info({times, currentTime}, 'update times info');
+}
+
+const sockServer = sockjs.createServer({ sockjs_url: 'http://127.0.0.1/js/sockjs.min.js' });
+sockServer.on('connection', conn => {
+    const connectionId = uuid.v4();
+    connections[connectionId] = conn;
+
+    conn.on('data', message => {
+        try {
+            message = JSON.parse(message);
+        } catch(err) {
+            logger.error('Failed to parse message: ' + message);
+            return;
+        }
+
+        if (message.type === 'updateTimeInfo') {
+            updateTimes(message.value);
+        }
+
+        if (message.type === 'play' || message.type === 'pause') {
+            sendToOthers(connectionId, {
+                type: message.type
+            });
+        }
+
+        if (message.type === 'setTime') {
+            currentTime = message.value;
+            times = [message.value];
+
+            sendToAll({
+                type  :  'setTime',
+                value : message.value
+            });
+        }
+
+        if (message.type === 'init') {
+            logger.info({connectionId}, 'init');
+
+            send(conn, {
+                type  : 'setTime',
+                value : currentTime
+            });
+        }
+    });
+
+    conn.on('close', () => {
+        delete connections[connectionId];
+    });
+});
+
+const server = http.createServer();
+sockServer.installHandlers(server, { prefix: '/echo' });
+server.listen(9999, '127.0.0.1');
